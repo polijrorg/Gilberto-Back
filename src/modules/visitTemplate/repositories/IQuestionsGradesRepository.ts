4db243546b45cacfd9012e8fdf6f2944aed4069a
Pedro Gomes <pedro.oliveiragti@gmail.com>
2024-07-29 12:43:37 -0300

    :bug: fix: Return Seller

--- src/modules/visitTemplate/repositories/IQuestionsGradesRepository.ts
+++ src/modules/visitTemplate/repositories/IQuestionsGradesRepository.ts
@@ -1,4 +1,4 @@
-import { QuestionsGrades } from '@prisma/client';
+import { QuestionsGrades, Seller } from '@prisma/client';
 
 import ICreateQuestionsGradesDTO from '../dtos/ICreateQuestionsGradesDTO';
 import IUpdateQuestionsGradesDTO from '../dtos/IUpdateQuestionsGradesDTO';
@@ -9,8 +9,8 @@ interface IQuestionsGradesRepository {
   getAllBySeller(sellerId: string): Promise<QuestionsGrades[] | null>;
   findById(id: string): Promise<QuestionsGrades | null>;
   update(id: string, data: IUpdateQuestionsGradesDTO): Promise<QuestionsGrades | null>;
-  getAllByIDSupervisor(idSupervisor: string): Promise<QuestionsGrades[] | null>;
-  getAllByIDManager(idManager: string): Promise<QuestionsGrades[] | null>;
+  getAllByIDSupervisor(idSupervisor: string): Promise<(QuestionsGrades & { seller: Seller })[] | null>;
+  getAllByIDManager(idManager: string): Promise<(QuestionsGrades & { seller: Seller })[] | null>;
 
 }
 
