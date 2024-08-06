2c06cbbfe3969e5e04b545a2d7dd551ed3a2148b
Gabriel Baraldi <baraldsbr@gmail.com>
2024-08-06 00:26:58 -0300

    :sparkles: feature: send action plan email

--- src/modules/actionPlans/repositories/IActionPlansRepository.ts
+++ src/modules/actionPlans/repositories/IActionPlansRepository.ts
@@ -4,7 +4,7 @@ import ICreateActionPlansDTO from '../dtos/ICreateActionPlansDTO';
 import IUpdateActionPlansDTO from '../dtos/IUpdateActionPlansDTO';
 
 interface IActionPlansRepository {
-  create(data: ICreateActionPlansDTO): Promise<ActionPlans>;
+  create(data: ICreateActionPlansDTO): Promise<ActionPlans & { seller: { email: string, name: string }, supervisor: { name: string } }>;
   delete(id: string): Promise<ActionPlans>;
   getAll(): Promise<ActionPlans[] | null>;
   getByIdSeller(idSeller: string): Promise<ActionPlans[] | null>;
